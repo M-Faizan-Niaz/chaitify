297b0b3b8061ab391db676f7b188bd9ab1f3cbe0
Faizan <faizaniaz17905@gmail.com>
2025-12-26 22:05:54 +0500

    inital commit

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,15 @@
+import express, { Router } from "express";
+
+
+const router = express.Router();
+
+
+router.get("/send", (req, res) => {
+    res.send("Send message endpoint");
+})
+
+router.get("/recieve", (req,res) => {
+    res.send("Recieve message endpoint")
+})
+
+export default router;
